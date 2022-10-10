13cd785d847c2c04f098ffc1edb400d62d16b43a
Костя Веселов <kveselov@ozon.ru>
2022-10-11 01:43:31 +0300

    rev-1

--- crypto/shuff-shuff/generator.py
+++ crypto/shuff-shuff/generator.py
@@ -2,7 +2,7 @@ import random
 
 flag = """
 How? Devise some method for "scoring" a piece of English plaintext. Character frequency is a good metric. Evaluate each output and choose the one with the best score.
-the answer is l c t f bracket five h u f f underscore five h u f f underscore five h u f f
+the answer is format bracket five h u f f underscore five h u f f underscore five h u f f
 """
 sd = random.randint(1, 10**5)
 random.seed(sd)
